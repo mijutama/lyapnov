7511ad061f7f1034bc7ab885b952e1b753a8567c
Miju <mizuki.miju@gmail.com>
2017-11-18 18:35:28 +0900

    more short code

--- rungekutta.h
+++ rungekutta.h
@@ -22,37 +22,22 @@ void rungekutta(Rungekutta<T> &t)
 {
 	const long n =  t.v.size();
 	vector<T> var;
-	vector<T> k[4];
+	vector<T> k[5];
 	var.resize(n);
-	for(int i = 0; i < 4; i++)
+	for(int i = 0; i < 5; i++)
 		k[i].resize(n);
 
-	// 1
-	for(int i = 0; i < n; i++)
-		var[i] = t.v[i];
-	for(int i = 0; i < n; i++)
-		k[0][i] = t.dt*t.f[i](var);
+  double a[4] {0.0, 0.5, 0.5, 1.0};
+  for(int i = 1 ; i < 5; i++)
+  {
+    for(int j = 0; j < n; j++)
+      var[j] = t.v[j]+a[i-1]*k[i-1][j];
+    for(int j = 0; j < n; j++)
+      k[i][j] = t.dt*t.f[j](var);
+  }
 
-	// 2
 	for(int i = 0; i < n; i++)
-		var[i] = t.v[i]+k[0][i]/2.0;
-	for(int i = 0; i < n; i++)
-		k[1][i] = t.dt*t.f[i](var);
-
-	// 3
-	for(int i = 0; i < n; i++)
-		var[i] = t.v[i]+k[1][i]/2.0;
-	for(int i = 0; i < n; i++)
-		k[2][i] = t.dt*t.f[i](var);
-
-	// 4
-	for(int i = 0; i < n; i++)
-		var[i] = t.v[i]+k[2][i];
-	for(int i = 0; i < n; i++)
-		k[3][i] = t.dt*t.f[i](var);
-	
-	for(int i = 0; i < n; i++)
-		t.v[i] += (k[0][i]+2.0*k[1][i]+2.0*k[2][i]+k[3][i])/6.0;
+		t.v[i] += (k[1][i]+2.0*k[2][i]+2.0*k[3][i]+k[4][i])/6.0;
 
 }
 
